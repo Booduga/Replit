34ce2d9ac69f3ac9f1d68d359eb420f023b89ae6
Booduga <>
2023-03-05 18:02:00 +0000

    Initial commit

---
+++ com.replit.inicial/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    MinhaCor minhaCor = new MinhaCor();
+    String cores = minhaCor.Principal("Verde");
+    System.out.println(cores);
+  }
+}
---
+++ com.replit.inicial/MinhaCor.java
@@ -0,0 +1,6 @@
+public class MinhaCor {
+  public String Principal(String pCor) {
+    String cor1 = pCor + " & " + "Amarelo";
+    return cor1;
+  }
+}
